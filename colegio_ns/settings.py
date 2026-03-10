9b126500ec2c9cf8edbf40417f8a8c804e5df98c
Joel <joeljjs100@gmail.com>
2026-03-10 01:36:17 -0300

    Fix directorios de archivos estaticos

--- colegio_ns/settings.py
+++ colegio_ns/settings.py
@@ -134,8 +134,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = []
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Media files (uploaded files)
 MEDIA_URL = '/media/'
